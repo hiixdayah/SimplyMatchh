789fd3596fd3145a81152292e9589bcd7bb3753d
Mindy Ho <mindyho77@gmail.com>
2018-05-28 10:51:24 +0800

    Initial commit

---
+++ app/src/main/java/com/example/mindy/simplymatch/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.mindy.simplymatch;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
